cae105b976388940fe7b3ed0bf0423d2eecbdeda
unknown <sallamtanna2015@hotmail.com>
2019-07-27 21:02:26 +0300

    change the formate of response for all end-points

--- src/controllers/getProduct.js
+++ src/controllers/getProduct.js
@@ -31,7 +31,7 @@ const getProduct = (req, res) => {
   Product.aggregate(query)
   .then(response =>{
     if(response.length > 0)
-      return res.json({ response: response, status: "ok" })
+      return res.json({ data: {categories: response}, metadata:{status: 'ok'}})
     return res.status(404).json({response:"not found"})
     })
   .catch(error => res.status(500).json({ status: 500, msg: "Server error" }));
