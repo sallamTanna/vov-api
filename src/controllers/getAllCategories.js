8c417afd4bb629f90006342a427f0c49f835913b
unknown <sallamtanna2015@hotmail.com>
2019-07-27 20:58:39 +0300

    change the response of /gategories end-point

--- src/controllers/getAllCategories.js
+++ src/controllers/getAllCategories.js
@@ -6,7 +6,7 @@ const getAllCategories = (req, res) => {
   Category.find()
   .then(response =>{
     if(response.length > 0)
-      return res.json({ data: response, status: "ok" })
+      return res.json({ data: {categories: response}, metadata:{status: 'ok'}})
     return res.status(404).json({response:"not found"})
     })
   .catch(error => res.status(500).json({ status: 500, msg: "Server error" }))
