43c5fce3bebd2549e3731c3968a11d93ef268ed2
unknown <sallamtanna2015@hotmail.com>
2019-07-27 21:25:18 +0300

    remove pagesNumber from the response

--- src/controllers/getAllProducts.js
+++ src/controllers/getAllProducts.js
@@ -21,7 +21,7 @@ const getAllProducts = (req, res) => {
     .skip(skip)
     .then(response =>{
       if(response.length > 0)
-        return res.json({ data: {products: response}, metadata:{status: 'ok', pagesNumber: Math.round(response.length/size)}})
+        return res.json({ data: {products: response}, metadata:{status: 'ok'}})
       return res.status(404).json({response:"not found"})
       })
     .catch(error => res.status(500).json({ status: 500, msg: "Server error" }));
