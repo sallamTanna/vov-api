be9071f6b094258e537d4c18f01cafa654197965
unknown <sallamtanna2015@hotmail.com>
2019-07-28 18:00:49 +0300

    Add pagesNumber inside the response of /categories/:category/products end-point

--- src/controllers/getProduct.js
+++ src/controllers/getProduct.js
@@ -1,7 +1,6 @@
 const { Product } = require('../models/product');
-let size = 2;
 
-const getProduct = (req, res) => {
+const getProduct = async(req, res) => {
 
   let  query = [
     {
@@ -14,7 +13,7 @@ const getProduct = (req, res) => {
     },
     {
       $match: {"requiredName.name": req.params.category}
-    }
+    },
   ]
 
   const page = +req.query.page || 1;
@@ -26,12 +25,16 @@ const getProduct = (req, res) => {
     query.push({$match: {$and: [{"requiredName.name": req.params.category}, {name: req.query.name}]}})
   }
 
+  let countQuery = [...query];
+  countQuery.push({$count: "count"})
+  const count = await Product.aggregate(countQuery);
+
   query.push({$skip:  size * (page -1)}, {$limit: size})
 
   Product.aggregate(query)
   .then(response =>{
     if(response.length > 0)
-      return res.json({ data: {products: response}, metadata:{status: 'ok'}})
+      return res.json({ data: {products: response}, metadata:{status: 'ok', pagesNumber: Math.ceil(count[0].count/size)}})
     return res.status(404).json({response:"not found"})
     })
   .catch(error => res.status(500).json({ status: 500, msg: "Server error" }));
