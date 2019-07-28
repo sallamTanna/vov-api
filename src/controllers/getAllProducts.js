04268633b0a87660943b9dc0c01534f1f4f98624
unknown <sallamtanna2015@hotmail.com>
2019-07-28 16:09:12 +0300

    Add pagesNumber to /products end-point response

--- src/controllers/getAllProducts.js
+++ src/controllers/getAllProducts.js
@@ -4,27 +4,26 @@ const {
 
 let query = {};
 
-const getAllProducts = (req, res) => {
+const getAllProducts = async(req, res) => {
 
   const page = +req.query.page || 1;
   const size = +req.query.size || 3;
   const skip = size * (page - 1);
 
-  if (req.query.name) {
+  if(req.query.name) {
     query = {
       name: req.query.name
-    };
+    }
   }
+  const count = await Product.count(query);
 
   Product.find(query)
-    .limit(size)
-    .skip(skip)
-    .then(response =>{
-      if(response.length > 0)
-        return res.json({ data: {products: response}, metadata:{status: 'ok'}})
-      return res.status(404).json({response:"not found"})
-      })
-    .catch(error => res.status(500).json({ status: 500, msg: "Server error" }));
+  .then(response =>{
+    if(response.length>0)
+      return res.json({ data: {products: response}, metadata:{status: 'ok', pagesNumber: Math.ceil(count/size)}})
+    return res.status(404).json({response:"not found"})
+  })
+  .catch(error => res.status(500).json({ status: 500, msg: "Server error" }));
 };
 
 module.exports = {
